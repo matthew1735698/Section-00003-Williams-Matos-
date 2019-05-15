93e1d6b18aa1a8eb1ba135e0ea5e5c4539001ffb
alexmatos009 <alexmatos009@gmail.com>
2019-05-14 22:04:27 -0400

    Implemented percent error

--- data/biomagnificationTermProject.java
+++ data/biomagnificationTermProject.java
@@ -19,46 +19,29 @@ public class biomagnificationTermProject {
 	// Array storing the average BMF values of each trophic levels
 	public static final double[] BMF = {0.393, 17.435, .16};
 
+	// Array storing the experimental values of MeHg concentrations found from research in the field (for error calculation)
+	public static final double[] MeHgExperimental = {0.09, 0.05, 0.4, 0.5, 0.5, 5.1, 5.3, 6.9, 17.4, 83.9, 54.6, 27.1, 27.2};
 
-	public static double planktonAbsorption(double MeHgInWaterFormula, double planktonsize)
-	{
-
+	public static double planktonAbsorption(double MeHgInWaterFormula, double planktonsize){
 		return 1000*1.692*Math.pow(planktonsize,-0.344)*MeHgInWaterFormula*0.1;
 	}
 
-	public static double trophicFunction(double Nitrogen15)
-	{
-
+	//This function calculates the trophic level of an organism based on its N15 level
+	public static double trophicFunction(double Nitrogen15){
 		return 1+((Nitrogen15 +0.03)/3.40);
-
 	}
 
 
 public static void main(String[] args)
 {
-
-	// opening file to store data points
-
-				String filename = "Simulation Biomagnification.txt";
-        PrintWriter outputFile = null;
-        try
-        {
-            outputFile = new PrintWriter(new FileOutputStream(filename,false));
-        }
-        catch(FileNotFoundException e)
-        {
-            System.out.println("File error.  Program aborted.");
-            System.exit(0);
-        }
-
-
-/** Initialize time-step, total length of simulation, the initial concentration of MeHg in each organism,
- the different rates of change, arrays for storing toxicity levels over time, and the concentration of Hg
-*/
+	/** Initialize time-step, total length of simulation, the initial concentration of MeHg in each organism,
+	 the different rates of change, arrays for storing toxicity levels over time, and the concentration of Hg
+	*/
 
 	double timeStep = 0.1;
 	double simulationLength = 100;                                                   // Length of the simulation (in days)
 	double currentYear = 219;
+	int yearCounter = 0;
 	double MeHgInitial = 0.001927;                                                   // pcg/L of mercury in 1800s
 	double MeHgInWaterFormula = MeHgInitial*Math.pow(10, (0.01589*currentYear));     // Magnification factor comparing concentration of MeHg in organisms vs water through bioaccumulation
   double MeHgofT1 = 0;
@@ -68,45 +51,99 @@ public static void main(String[] args)
   double netPlanktonsizeT1 = 	88E-6;
   double netPlanktonsizeT2 = 	875E-6;
 	double MeHgofT1andT2 = MeHgofT1 + MeHgofT2;
+	int year = 365;
 
 
 
+	//  Write out each of the differential equations & Euler's method
+			int N = (int) (simulationLength/timeStep) + 1;
 
-//  Write out each of the differential equations & Euler's method
-	   	int N = (int) (simulationLength/timeStep) + 1;
+				for (int j = 0; j < N; j++) {
 
-			for (int l = 1; l < N; l++)
+					MeHgofT1 = planktonAbsorption(MeHgInWaterFormula, netPlanktonsizeT1);
 
-			{
+					MeHgofT2 = planktonAbsorption(MeHgInWaterFormula, netPlanktonsizeT2);
 
-				MeHgofT1 = planktonAbsorption(MeHgInWaterFormula, netPlanktonsizeT1);
+					MeHgofT3 = MeHgofT3 + (BMF[1] * MeHgofT1andT2 * timeStep);
 
-				MeHgofT2 = planktonAbsorption(MeHgInWaterFormula, netPlanktonsizeT2);
+					MeHgofT4 = MeHgofT4 + (BMF[2] * MeHgofT3 * timeStep);
 
-				MeHgofT3 = MeHgofT3 + (BMF[1] * MeHgofT1andT2 * timeStep);
+					MeHgofT1andT2 = MeHgofT1 + MeHgofT2;
 
-				MeHgofT4 = MeHgofT4 + (BMF[2] * MeHgofT3 * timeStep);
+					yearCounter++;
 
-				MeHgofT1andT2 = MeHgofT1 + MeHgofT2;
-
-				currentYear++;
-
-			}
-
-		// converts concentration from picograms/L to nanograms/gram
-
-		double MeHgngwgT1 = MeHgofT1*0.0000001;
-		double MeHgngwgT2 = MeHgofT2*0.0000001;
-		double MeHgngwgT3 = MeHgofT3*0.0000001;
-		double MeHgngwgT4 = MeHgofT4*0.0000001;
-
-		System.out.println("The average level of MeHg at T4 in ng/g is : " +MeHgngwgT4);
-		System.out.println("The average level of MeHg at T3 in ng/g is : " +MeHgngwgT3);
-		System.out.println("The average level of MeHg at T2 in ng/g is : " +MeHgngwgT2);
-		System.out.println("The average level of MeHg at T1 in ng/g is : " +MeHgngwgT1);
+					if (yearCounter % year == 0){ // This increase the current year for the calculation of concentration of MeHg in the plankton
+						currentYear++;
+					}
 
 
+					// converts concentration from picograms/L to nanograms/gram
+							System.out.println(" ");
+					System.out.println("//////////////////////////////////");
+					System.out.println("-------------Loop# "+j+"------------");
+					System.out.println("//////////////////////////////////");
+					System.out.println(" ");
+					double MeHgngwgT1 = MeHgofT1*0.0000001;
+					double MeHgngwgT2 = MeHgofT2*0.0000001;
+					double MeHgngwgT3 = MeHgofT3*0.0000001;
+					double MeHgngwgT4 = MeHgofT4*0.0000001;
 
+					System.out.println("The average level of MeHg at T4 in ng/g is : " +MeHgngwgT4);
+					System.out.println("The average level of MeHg at T3 in ng/g is : " +MeHgngwgT3);
+					System.out.println("The average level of MeHg at T2 in ng/g is : " +MeHgngwgT2);
+					System.out.println("The average level of MeHg at T1 in ng/g is : " +MeHgngwgT1);
+
+					System.out.println(" ");
+					System.out.println(" ");
+
+
+					// Trophic level based on the concentratoin of N15 in a subset of
+					// organisms from the Bay of Fundy
+
+					double[] TL = new double[13];
+
+					for (int i = 0; i < TL.length && i <= N15.length; i++) {
+
+					double tlevel = trophicFunction(N15[i]);
+					double roundedtlevel = Math.round (tlevel*100)/ 100.0;
+					TL [i] = roundedtlevel;
+
+					}
+
+
+					// Converting the trophic level of the organisms that was calculated
+					// to estimated MeHg concenctrations
+
+					System.out.println(" ");
+					System.out.println(" ");
+					double[] MEHG = new double[13];
+
+					for (int i = 0; i < MEHG.length; i++) {
+
+						double mehg = 0.0105*Math.pow(euler(), TL[i]*1.6839);
+						int nar = i+1;
+						MEHG[i] = mehg;
+						System.out.println("MeHg of orgnanism "+nar+" " +MEHG[i]);
+						System.out.println(" ");
+						System.out.println("Trophic level of organism "+nar+" " +TL[i]);
+						System.out.println(" ");
+						System.out.println(" ");
+					}
+
+	// Checking % error in our model by comparing the actual values of MeHg and those estimated by our model
+			System.out.println(" ");
+			System.out.println(" ");
+			double [] MeHgerror = new double [13];
+
+							for (int i = 0; i < MEHG.length; i++){
+
+							int var = i+1;
+
+							MeHgerror[i] = Math.abs(100-(((Math.abs(MeHgExperimental[i] - MEHG[i]))/MeHgExperimental[i])*100));
+
+							System.out.println("The % error for the organism number "+var+" is: "+MeHgerror[i]);
+
+							}
 		//Print final values to file
 	   outputFile.printf("%6.1f\t%6.1f\t%6.1f\t%6.1f", MeHgofT1, MeHgofT2, MeHgofT3, MeHgofT4);
 
